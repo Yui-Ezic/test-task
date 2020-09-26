e1b790690707c34f7571ece3adf0619a0a9f4abb
Yui-Ezic <mishan221199@gmail.com>
2020-09-26 14:20:16 +0300

    Added global exception handling for api

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -5,12 +5,10 @@ namespace App\Controller;
 
 use App\Entity\User\Id;
 use App\Entity\User\User;
-use App\Repository\EntityNotFoundException;
 use App\Repository\User\UserRepository;
 use App\UseCase\User\Create;
-use App\UseCase\User\Update;
 use App\UseCase\User\Delete;
-use DomainException;
+use App\UseCase\User\Update;
 use JsonException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -41,16 +39,8 @@ class UsersController
     {
         $users = $this->repository->getAll();
 
-        $response['items'] = array_map(static function (User $user) {
-            return [
-                'id' => $user->getId()->getValue(),
-                'name' => [
-                    'first' => $user->getName()->getFirst(),
-                    'last' => $user->getName()->getLast(),
-                    'full' => $user->getName()->getFull()
-                ],
-                'email' => $user->getEmail()->getValue()
-            ];
+        $response['items'] = array_map(function (User $user) {
+            return $this->convertUserToArray($user);
         }, $users);
 
         return new JsonResponse($response);
@@ -61,40 +51,31 @@ class UsersController
      * @param Request $request
      * @param Create\Handler $handler
      * @return Response
+     * @throws JsonException
      */
     public function create(Request $request, Create\Handler $handler): Response
     {
-        try {
-            $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
+        $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
 
-            $firstName = $data['first_name'] ?? null;
-            $lastName = $data['last_name'] ?? null;
-            $email = $data['email'] ?? null;
-            $password = $data['password'] ?? null;
+        $firstName = $data['first_name'] ?? null;
+        $lastName = $data['last_name'] ?? null;
+        $email = $data['email'] ?? null;
+        $password = $data['password'] ?? null;
 
-            if (empty($firstName) || empty($lastName) || empty($email) || empty($password)) {
-                return new JsonResponse([
+        if (empty($firstName) || empty($lastName) || empty($email) || empty($password)) {
+            return new JsonResponse([
+                'error' => [
                     'status' => 400,
                     'message' => 'One of required parameters is not set or empty.'
-                ], 400);
-            }
-
-            $command = new Create\Command($firstName, $lastName, $email, $password);
-
-            $handler->handle($command);
-
-            return new JsonResponse(['status' => 'ok'], Response::HTTP_CREATED);
-        } catch (JsonException $e) {
-            return new JsonResponse([
-                'status' => Response::HTTP_BAD_REQUEST,
-                'message' => 'Invalid json'
-            ], Response::HTTP_BAD_REQUEST);
-        } catch (DomainException $exception) {
-            return new JsonResponse([
-                'status' => Response::HTTP_BAD_REQUEST,
-                'message' => $exception->getMessage()
-            ], Response::HTTP_BAD_REQUEST);
+                ]
+            ], 400);
         }
+
+        $command = new Create\Command($firstName, $lastName, $email, $password);
+
+        $handler->handle($command);
+
+        return new JsonResponse(['success' => true], Response::HTTP_CREATED);
     }
 
     /**
@@ -104,24 +85,9 @@ class UsersController
      */
     public function show(string $id): Response
     {
-        try {
-            $user = $this->repository->get(new Id($id));
+        $user = $this->repository->get(new Id($id));
 
-            return new JsonResponse([
-                'id' => $user->getId()->getValue(),
-                'name' => [
-                    'first' => $user->getName()->getFirst(),
-                    'last' => $user->getName()->getLast(),
-                    'full' => $user->getName()->getFull()
-                ],
-                'email' => $user->getEmail()->getValue()
-            ]);
-        } catch (EntityNotFoundException $exception) {
-            return new JsonResponse([
-                'status' => Response::HTTP_NOT_FOUND,
-                'message' => $exception->getMessage()
-            ], Response::HTTP_NOT_FOUND);
-        }
+        return new JsonResponse($this->convertUserToArray($user));
     }
 
     /**
@@ -130,37 +96,21 @@ class UsersController
      * @param string $id
      * @param Update\Handler $handler
      * @return Response
+     * @throws JsonException
      */
     public function update(Request $request, string $id, Update\Handler $handler): Response
     {
-        try {
-            $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
+        $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
 
-            $command = new Update\Command($id);
-            $command->firstName = $data['first_name'] ?? null;
-            $command->lastName = $data['last_name'] ?? null;
-            $command->email = $data['email'] ?? null;
-            $command->password = $data['password'] ?? null;
+        $command = new Update\Command($id);
+        $command->firstName = $data['first_name'] ?? null;
+        $command->lastName = $data['last_name'] ?? null;
+        $command->email = $data['email'] ?? null;
+        $command->password = $data['password'] ?? null;
 
-            $handler->handle($command);
+        $handler->handle($command);
 
-            return new JsonResponse(['status' => 'ok']);
-        } catch (JsonException $e) {
-            return new JsonResponse([
-                'status' => Response::HTTP_BAD_REQUEST,
-                'message' => 'Invalid json'
-            ], Response::HTTP_BAD_REQUEST);
-        } catch (EntityNotFoundException $exception) {
-            return new JsonResponse([
-                'status' => Response::HTTP_NOT_FOUND,
-                'message' => $exception->getMessage()
-            ], Response::HTTP_NOT_FOUND);
-        } catch (DomainException $exception) {
-            return new JsonResponse([
-                'status' => Response::HTTP_BAD_REQUEST,
-                'message' => $exception->getMessage()
-            ], Response::HTTP_BAD_REQUEST);
-        }
+        return new JsonResponse(['success' => true]);
     }
 
     /**
@@ -171,16 +121,26 @@ class UsersController
      */
     public function delete(string $id, Delete\Handler $handler): Response
     {
-        try {
-            $command = new Delete\Command($id);
-            $handler->handle($command);
+        $command = new Delete\Command($id);
+        $handler->handle($command);
 
-            return new JsonResponse(['status' => 'ok']);
-        } catch (EntityNotFoundException $exception) {
-            return new JsonResponse([
-                'status' => Response::HTTP_NOT_FOUND,
-                'message' => $exception->getMessage()
-            ], Response::HTTP_NOT_FOUND);
-        }
+        return new JsonResponse(['success' => true]);
+    }
+
+    /**
+     * @param User $user
+     * @return array
+     */
+    private function convertUserToArray(User $user): array
+    {
+        return [
+            'id' => $user->getId()->getValue(),
+            'name' => [
+                'first' => $user->getName()->getFirst(),
+                'last' => $user->getName()->getLast(),
+                'full' => $user->getName()->getFull()
+            ],
+            'email' => $user->getEmail()->getValue()
+        ];
     }
 }
---
+++ src/EventListener/ExceptionHandler.php
@@ -0,0 +1,72 @@
+<?php
+
+
+namespace App\EventListener;
+
+use App\Repository\EntityNotFoundException;
+use DomainException;
+use JsonException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Throwable;
+
+class ExceptionHandler implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return array(
+            KernelEvents::EXCEPTION => 'onKernelException'
+        );
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $throwable = $event->getThrowable();
+        $request = $event->getRequest();
+
+        if (in_array('application/json', $request->getAcceptableContentTypes(), true)) {
+            $event->setResponse($this->createJsonResponse($throwable));
+        }
+    }
+
+    public function createJsonResponse(Throwable $throwable): JsonResponse
+    {
+        if ($throwable instanceof JsonException) {
+            return new JsonResponse([
+                'error' => [
+                    'status' => Response::HTTP_BAD_REQUEST,
+                    'message' => 'Invalid json'
+                ]
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($throwable instanceof DomainException) {
+            return new JsonResponse([
+                'error' => [
+                    'status' => Response::HTTP_BAD_REQUEST,
+                    'message' => $throwable->getMessage()
+                ]
+            ]);
+        }
+
+        if ($throwable instanceof EntityNotFoundException) {
+            return new JsonResponse([
+                'status' => Response::HTTP_NOT_FOUND,
+                'message' => $throwable->getMessage()
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $status = $throwable instanceof HttpExceptionInterface ? $throwable->getStatusCode() : Response::HTTP_INTERNAL_SERVER_ERROR;
+
+        return new JsonResponse([
+            'error' => [
+                'status' => $status,
+                'message' => 'Unknown error'
+            ]
+        ]);
+    }
+}
